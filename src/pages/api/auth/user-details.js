6b69e957998a6ad7ad21ad688b4b3d9c1f85a871
JorryBiksu <jorryc01@gmail.com>
2023-12-01 15:57:24 +0700

    update auth/user-details

--- src/pages/api/auth/user-details.js
+++ src/pages/api/auth/user-details.js
@@ -1,5 +1,5 @@
 import { cors, runMiddleware } from "@/helpers/cors-middleware";
-import prisma from "../../../prisma/client";
+import prisma from "../../../../prisma/client";
 
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
