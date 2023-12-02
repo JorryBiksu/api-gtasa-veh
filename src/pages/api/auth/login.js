421b338165964744654be0df8edff1a640b0b2dd
JorryBiksu <jorryc01@gmail.com>
2023-12-02 10:34:22 +0700

    update api/auth/login

--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -1,21 +1,28 @@
-import { runMiddleware } from "@/helpers/cors-middleware";
-import authenticateUser from "@/controllers/authController"; // Sesuaikan path ini
+// pages/api/login.js
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  await runMiddleware(req, res, cors);
-
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const { username, password } = req.body;
 
-    try {
-      const user = await authenticateUser(username, password);
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
 
-      // Generate token or handle successful authentication as needed
-      res.status(200).json({ message: "Login successful", user });
-    } catch (e) {
-      res.status(401).json({ message: "Invalid credentials" });
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({ message: 'Invalid credentials' });
     }
+
+    const token = jwt.sign({ userId: user.id }, '5965597916', {
+      expiresIn: '1h',
+    });
+
+    res.status(200).json({ token });
   } else {
-    res.status(405).json({ message: "Method not allowed" });
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
 }
