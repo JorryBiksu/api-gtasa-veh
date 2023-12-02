fd80b17428c54add327076d1cf275bf3f593e545
JorryBiksu <jorryc01@gmail.com>
2023-12-02 18:22:17 +0700

    add cors 1.1

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/auth/login",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
