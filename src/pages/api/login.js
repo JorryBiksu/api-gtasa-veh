9fc0daa5575fae5f39c3cc1f4b38527de22cfc5e
JorryBiksu <jorryc01@gmail.com>
2023-12-04 19:05:32 +0700

    login

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,35 +1,26 @@
-// pages/api/login.js
+// pages/api/login.
 import { generateToken } from "@/helpers/authentication";
 import { cors, runMiddleware } from "@/helpers/cors-middleware";
 import prisma from "../../../prisma/client";
 import bcrypt from 'bcryptjs';
 import Cookies from 'js-cookie';
 
-export default async function handler(req, res) {
-  await runMiddleware(req, res, cors);
 
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors)
   if (req.method === 'POST') {
     const { username, password } = req.body;
 
-    try {
-      const user = await prisma.user.findUnique({
-        where: { username },
-      });
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
 
-      if (!user || !bcrypt.compareSync(password, user.password)) {
-        return res.status(401).json({ message: 'Invalid credentials' });
-      }
-
-      // Cookie kedaluwarsa dalam 1 jam
-      res.status(200).json({ message: 'Login successful' });
-    } catch (error) {
-      console.error('Prisma Error:', error);
-      res.status(500).json({ message: 'Internal Server Error' });
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({ message: 'Invalid credentials' });
     }
-  } else if (req.method === 'GET') {
-    // Penanganan permintaan GET jika diperlukan
-    // Misalnya, memberikan informasi publik atau statistik
-    res.status(200).json({ message: 'GET request successful' });
+ // Cookie kedaluwarsa dalam 1 jam
+
+    res.status(200).json({ message: 'Login successful' });
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
   }
