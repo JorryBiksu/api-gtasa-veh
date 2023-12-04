48737f321f440c551f6631fd8e2a8acdb65eb09c
JorryBiksu <jorryc01@gmail.com>
2023-12-04 18:51:13 +0700

    update JWT

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -18,11 +18,7 @@ export default async function handler(req, res) {
     if (!user || !bcrypt.compareSync(password, user.password)) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
-
-    const token = generateToken(user.id);
-
-    // Set token sebagai cookie di sisi klien
-    Cookies.set('token', token, { expires: 1 / 24 }); // Cookie kedaluwarsa dalam 1 jam
+ // Cookie kedaluwarsa dalam 1 jam
 
     res.status(200).json({ message: 'Login successful' });
   } else {
